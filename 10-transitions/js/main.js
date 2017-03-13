fca3d6b2ce5ccb8760fc3d9441d37a8709c435ec
Juan Esquivias <juanesquiviasotazu@gmail.com>
2017-03-13 12:03:08 -0500

    transitions

---
+++ 10-transitions/js/main.js
@@ -0,0 +1,12 @@
+new Vue({
+	el: 'main',
+	data: {
+		mostrar: true,
+		mensajes: {
+			transicion: 'Transition CSS with Vue.js',
+			animacion: 'Animation CSS with Vue.js',
+			animationCustom: 'Animations custom CSS with Vue.js',
+			entreElementos: 'Transitions between CSS with Vue.js',
+		}
+	},
+});
