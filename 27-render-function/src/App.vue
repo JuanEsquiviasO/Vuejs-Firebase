797bd6180caf28a99abfc36364ba6627658a572f
Juan Esquivias <juanesquiviasotazu@gmail.com>
2017-04-04 12:22:13 -0500

    render function

---
+++ 27-render-function/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<elemento tipo="footer">Hi footer!</elemento>
+		<elemento tipo="header">Hi header!</elemento>
+		<elemento tipo="aside">Hi aside!</elemento>
+		<elemento tipo="nav">Hi nav!</elemento>	
+		<elemento tipo="main">Hi main!</elemento>	
+	</div>
+</template>
+
+<script>
+	import elemento from './components/Elemento.vue';
+	export default {
+		components: { elemento }
+	}
+</script>
+
+<style>
+</style>
