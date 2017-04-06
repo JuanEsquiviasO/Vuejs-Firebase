8868ec5791dc515e61953203719f90e8c15cf20e
Juan Esquivias <juanesquiviasotazu@gmail.com>
2017-04-06 15:14:20 -0500

    scoped css

---
+++ 29-scoped-css/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<h1>Main menu</h1>
+		<input type="range">
+		<ul>
+			<li><a href="">Link 1</a></li>
+			<li><a href="">Link 2</a></li>
+			<li><a href="">Link 3</a></li>
+			<li><a href="">Link 4</a></li>
+			<li><a href="">Link 5</a></li>
+			<li><a href="">Link 6</a></li>
+		</ul>
+		<tareas></tareas>
+	</div>
+</template>
+
+<script>
+	import Tareas from './components/Tareas.vue';
+	export default {
+		components: {Tareas},
+	}
+</script>
+
---
+++ 29-scoped-css/src/components/Tareas.vue
@@ -0,0 +1,51 @@
+<template>
+	<div>
+		<h2>Tasks to do</h2>
+		<ul>
+			<li v-for="tarea in tareasPendientes">
+				<a href="#" @click="actualizarTarea(tarea)" v-text="tarea.nombre"></a>
+			</li>
+		</ul>
+		<h2>Completed Tasks</h2>
+		<li v-for="tarea in tareasFinalizadas">
+			<a href="#" @click="actualizarTarea(tarea)" v-text="tarea.nombre"></a>
+		</li>
+	</div>
+</template>
+<script>
+	export default {
+		data(){
+			return {
+				tareas: [
+					{nombre: 'Task 1', completado: false},
+					{nombre: 'Task 2', completado: false},
+					{nombre: 'Task 3', completado: false},
+					{nombre: 'Task 4', completado: false},
+					{nombre: 'Task 5', completado: false},
+					{nombre: 'Task 6', completado: false},
+					{nombre: 'Task 7', completado: false},
+					{nombre: 'Eat my biscuits', completado: false},
+				],
+				finalizadas: []
+			}
+		},
+		methods: {
+			actualizarTarea(tarea) {
+				tarea.completado = !tarea.completado;
+			}
+		},
+		computed: {
+			tareasPendientes() {
+				return this.tareas.filter((tarea) => !tarea.completado);
+			},
+			tareasFinalizadas() {
+				return this.tareas.filter((tarea) => tarea.completado);
+			}
+		}
+	}
+</script>
+<style scoped >
+	li a {
+		color: peru;
+	}
+</style>
