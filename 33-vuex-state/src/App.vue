349fa433d4c0ebac3aa1696fee4cbc9d289a2e39
Juan Esquivias <juanesquiviasotazu@gmail.com>
2017-04-12 16:25:52 -0500

    vuex state tree

---
+++ 33-vuex-state/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<h1>Hi my name is {{ nombre }} {{ apellidos }} and I am {{ profesion }}</h1>
+</template>
+
+<script>
+	import {mapState} from 'vuex';
+	export default {
+		// computed: {
+		// 	nombre() {
+		// 		return this.$store.state.nombre;
+		// 	}
+		// }
+		// computed: mapState({
+		// 	nombre: (state) => state.nombre,
+		// 	apellidos: (state) => state.apellidos,
+		// 	profesion: (state) => state.profesion,
+		// 	ciudad: (state) => state.ciudad
+		// })
+		computed: mapState(['nombre', 'apellidos', 'profesion']),
+	}
+</script>
+
